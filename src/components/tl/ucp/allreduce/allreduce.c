e3576a5ae3f3ec8b544864b473405734b3a5228a
Nick Sarkauskas <nsarkauskas@nvidia.com>
2024-01-23 09:36:37 -0800

    TL/UCP: Add stubs for sliding window allreduce

--- src/components/tl/ucp/allreduce/allreduce.c
+++ src/components/tl/ucp/allreduce/allreduce.c
@@ -20,6 +20,10 @@ ucc_base_coll_alg_info_t
              .name = "sra_knomial",
              .desc = "recursive knomial scatter-reduce followed by knomial "
                      "allgather (optimized for BW)"},
+        [UCC_TL_UCP_ALLREDUCE_ALG_SLIDING_WINDOW] =
+            {.id   = UCC_TL_UCP_ALLREDUCE_ALG_SLIDING_WINDOW,
+             .name = "sliding_window",
+             .desc = "sliding window allreduce (optimized for running on DPU)"},
         [UCC_TL_UCP_ALLREDUCE_ALG_LAST] = {
             .id = 0, .name = NULL, .desc = NULL}};
 
@@ -45,4 +49,12 @@ ucc_status_t ucc_tl_ucp_allreduce_knomial_init(ucc_base_coll_args_t *coll_args,
     status = ucc_tl_ucp_allreduce_knomial_init_common(task);
 out:
     return status;
+}
+
+ucc_status_t
+ucc_tl_ucp_allreduce_sliding_window_init(ucc_base_coll_args_t *coll_args,
+                                         ucc_base_team_t *     team,
+                                         ucc_coll_task_t **    task_h)
+{
+    return UCC_OK;
 }
